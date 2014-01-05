a60b8071fe8c8dc66fc5ec5dcc6bbdab93e5b207
Daniel Suo <danielsuo@gmail.com>
2014-01-04 23:46:05 -0500

    add edit

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -12,7 +12,22 @@ Router.map(function() {
 		data: function() { return Posts.findOne(this.params._id); }
 	});
 
+	this.route('postEdit', {
+		path: 'posts/:_id/edit',
+		data: function() { return Posts.findOne(this.params._id); }
+	});
+
 	this.route('postSubmit', {
 		path: '/submit'
 	});
 });
+
+var requireLogin = function() {
+	if (! Meteor.user()) {
+		if (Meteor.loggingIn()) this.render(this.loadingTemplate);
+		else this.render('accessDenied');
+		this.stop();
+	}
+};
+
+Router.before(requireLogin, {only: 'postSubmit'});
--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,40 @@
 Posts = new Meteor.Collection('posts');
+
+Posts.allow({
+	update: ownsDocument, 
+	remove: ownsDocument
+});
+
+Posts.deny({
+	update: function(userId, post, fieldNames) {
+		return (_.without(fieldNames, 'url', 'title').length > 0);
+	}
+});
+
+Meteor.methods({
+	post: function(postAttributes) {
+		var user = Meteor.user();
+		var postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+		if (!user)
+			throw new Meteor.Error(401, 'You need to login to post new stories.');
+
+		if (!postAttributes.title)
+			throw new Meteor.Error(422, 'Please fill in a headline.');
+
+		if (postAttributes.url && postWithSameLink) {
+			throw new Meteor.Error(302, 'This link has already been posted',
+				postWithSameLink._id);
+		}
+
+		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+
+		var postId = Posts.insert(post);
+
+		return postId;
+	}
+});
